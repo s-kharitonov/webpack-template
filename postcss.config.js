cd137fcb4508851fc9345d542517169b45c1fc9a
SKdeer <SKdeer13@gmail.com>
2019-01-15 23:58:14 +0300

    add autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,15 @@
 module.exports = {
     plugins: [
-        require('autoprefixer')
+        require('autoprefixer'),
+        require('css-mqpacker'),
+        require('cssnano')({
+            preset: [
+                'default', {
+                    discardComments: {
+                        removeAll: true,
+                    }
+                }
+            ]
+        })
     ]
 }
